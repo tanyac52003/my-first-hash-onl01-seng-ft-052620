dd025d55b554ab62acc8cb70d4ed391f5b0560dc
Tanya Hodges <tanyac52003@gmail.com>
2020-06-01 21:21:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,8 +37,8 @@ def adding
                   "oil paintings" => 3
                  }
 
-shipping_manifest["muskets" => 2]
-shipping_manifest["gun powder" => 4]
+shipping_manifest["muskets"] = 2 
+shipping_manifest["gun powder"] = 4 
 shipping_manifest
 
 
